5ff55fbd5f2a1d4543f13ac8e8f89e8e0b3f4db0
Mohammad Haolder <73881636+saadafahmed45@users.noreply.github.com>
2023-08-08 01:39:48 +0600

    Update newsSingle.js

--- src/api/newsSingle.js
+++ src/api/newsSingle.js
@@ -3,7 +3,7 @@ const newsSingle = async (id) => {
    const response = await fetch(`https://api.worldnewsapi.com/search-news?api-key=67715fcebbe445d8aad2f379896d7a1a&text=tesla/${id}`);
 
    const data = response.json();
-   return data;
+   return data.news[0];
 
 };
 
